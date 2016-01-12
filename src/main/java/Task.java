6a75b7d0cda908174935f0ae7c363bdb4b7685bd
Chris Houghton and Nathan Romike <cehoughton71@gmail.com>
2016-01-12 11:24:54 -0800

    Add initial files.

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class Tamagotchi {
+
+}
